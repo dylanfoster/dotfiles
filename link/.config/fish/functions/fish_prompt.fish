7bf297fdb42d286ddf1adadac132c64c75e4b047
Dylan Foster <dylan_foster@apple.com>
2015-02-11 16:05:12 -0800

    updates fish_prompt

--- link/.config/fish/functions/fish_prompt.fish
+++ link/.config/fish/functions/fish_prompt.fish
@@ -1,65 +1,67 @@
-set -g __fish_git_prompt_show_informative_status 1
-set -g __fish_git_prompt_hide_untrackedfiles 1
+function _git_branch_name
+  echo (command git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||')
+end
 
-set -g __fish_git_prompt_color_branch magenta bold
-set -g __fish_git_prompt_showupstream "informative"
-set -g __fish_git_prompt_char_upstream_ahead "↑"
-set -g __fish_git_prompt_char_upstream_behind "↓"
-set -g __fish_git_prompt_char_upstream_prefix ""
-
-set -g __fish_git_prompt_char_stagedstate "●"
-set -g __fish_git_prompt_char_dirtystate "+"
-set -g __fish_git_prompt_char_untrackedfiles "…"
-set -g __fish_git_prompt_char_conflictedstate "✖"
-set -g __fish_git_prompt_char_cleanstate "✔"
-
-set -g __fish_git_prompt_color_dirtystate blue
-set -g __fish_git_prompt_color_stagedstate yellow
-set -g __fish_git_prompt_color_invalidstate red
-set -g __fish_git_prompt_color_untrackedfiles $fish_color_normal
-set -g __fish_git_prompt_color_cleanstate green bold
-
-function fish_prompt --description 'Write out the prompt'
+function fish_prompt
   set -l last_status $status
-  set -l status_color
-  set -l identity (whoami)'@'(hostname)
-  set -l git_prompt (__fish_git_prompt "|")
-  set -l formatted_path ' : '(pwd | sed "s|$HOME|~|")
 
-  set -g current_branch (git rev-parse --abbrev-ref HEAD 2> /dev/null)
+  set -l normal (set_color normal)
+  set -l blue (set_color blue)
+  set -l cyan (set_color cyan)
+  set -l green (set_color green)
+  set -l magenta (set_color magenta)
+  set -l purple (set_color purple)
+  set -l red (set_color red)
+  set -l yellow (set_color yellow)
 
-  if not set -q __fish_prompt_normal
-    set -g __fish_prompt_normal (set_color normal)
-  end
+  set -l cwd $cyan(pwd | sed "s:^$HOME:~:")
+  set -l prefix '$ ' $normal
 
-  if not test $last_status -eq 0
-    set status_color $fish_color_error
-  else
-    set status_color '--bold green'
-  end
+  # fish git prompt settings
+  set -g __fish_git_prompt_char_dirtystate '+'
+  set -g __fish_git_prompt_char_stagedstate '•'
+  set -g __fish_git_prompt_char_stateseparator '|'
+  set -g __fish_git_prompt_color_branch yellow
+  set -g __fish_git_prompt_color_upstream_ahead green
+  set -g __fish_git_prompt_color_upstream_behind red
+  set -g __fish_git_prompt_hide_untrackedfiles 1
+  set -g __fish_git_prompt_show_informative_status 1
+  set -g __fish_git_prompt_showcolorhints 1
+  set -g __fish_git_prompt_showdirtystate 'yes'
+  set -g __fish_git_prompt_showstashstate 'yes'
+  set -g __fish_git_prompt_showuntrackedfiles 'yes'
+  set -g __fish_git_prompt_showupstream 'informative'
 
+  # new line before each prompt
   echo
 
-  # current user and hostname
-  set_color --bold green
-  echo -n $identity
+  # show ssh status
+  if [ -n "$SSH_CONNECTION" ]
+    echo -ns $red '(ssh) ' $normal
+  end
 
-  # git prompt
-  set_color normal
-  echo -n $git_prompt
+  # show user and host
+  echo -ns $green (whoami) '@' (hostname) $normal
 
-  # current working directory
-  echo -n $formatted_path
+  # show git info
+  if [ (_git_branch_name) ]
+    __fish_git_prompt ' ❮%s⟫'
+  end
 
+  # show current working directory
+  echo -ns $normal' : '(pwd | sed "s:^$HOME:~:")
+
+  # new line after main line
   echo
 
-  # last return status
-  echo -n '['
-  eval "set_color $status_color"
-  echo -n $last_status
-  set_color normal
-  echo -n ']'
+  # show last exit status
+  echo -ns $normal '['
+  if [ $last_status -ne 0 ]
+    echo -ns $red
+  end
+  echo -ns $last_status
+  echo -ns $normal ']'
 
-  # all done!
-  echo -n ' $ '
+  # show prefix
+  echo -ns ' $ '
 end
