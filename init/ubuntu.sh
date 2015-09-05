64189f58f67c4e8e7c4eca79d71dc12e41c5f562
Dylan Foster <dylan947@gmail.com>
2015-09-05 05:15:36 -0700

    fix(init): remove old func reference in ubuntu script

--- init/ubuntu.sh
+++ init/ubuntu.sh
@@ -2,12 +2,12 @@
 is_ubuntu || return 1
 
 # Update APT
-notice "Updating APT"
+warn "Updating APT"
 sudo apt-get -qq update
 sudo apt-get -qq upgrade
 
 # Install APT packages
-notice "Installing APT packages: ${list[*]}"
+warn "Installing APT packages: ${list[*]}"
 sudo apt-get -qq install \
   autoconf \
   bc \
