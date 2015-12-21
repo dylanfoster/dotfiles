03c8152def919c66855e45b2d1e52db1fe650137
Dylan Foster <dylan947@gmail.com>
2015-12-21 13:04:40 -0800

    feat(tmux): add tmux detach alias

--- env/tmux.sh
+++ env/tmux.sh
@@ -2,6 +2,7 @@ alias tma='tmux attach -d -t'
 alias tmk='tmux kill-session -t'
 alias tmak='tmux kill-server'
 alias tml='tmux list-sessions'
+alias tmd='tmux detach'
 tmn() {
   if [ ! -z $argv ]; then
     name=$argv
