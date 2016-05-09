2284da0fcd58966e3ddd839eea78856b4d9eaba5
Dylan Foster <dylan947@gmail.com>
2016-05-09 14:42:53 -0700

    feat(git): add standup

--- env/git.sh
+++ env/git.sh
@@ -14,6 +14,7 @@ alias grv='gr -v'
 alias gs='g status'
 alias gu='g pull'
 alias gup='g pull --rebase'
+alias standup='git log --reverse --branches --since="last monday" --author=$(git config --get user.email) --format=format:"%C(cyan) %ad %C(yellow)%h %Creset %s %Cgreen%d" --date=local'
 
 # Shows files changed for a given path (gfc master..release)
 alias gfc='git diff-tree --no-commit-id --name-only -r'
